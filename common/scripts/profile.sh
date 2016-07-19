9a41729e417df6a8926d823e89537495df97477b
mansi <manaswini.maharana@gmail.com>
2016-07-19 10:02:58 -0400

    removing files

---
+++ common/scripts/profile.sh
@@ -0,0 +1,3 @@
+export nameNode=hdfs://quickstart.cloudera:8020
+export jobTracker=quickstart.cloudera:8032 
+export OOZIE_URL=http://quickstart.cloudera:11000/oozie/
---
+++ common/scripts/start.sh
@@ -0,0 +1,1 @@
+bash run_oozie_workflow.sh /user/cloudera/examples/apps/hive /home/cloudera/oozie-autosys/examples/apps/hive/job.properties
